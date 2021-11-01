0bceafd863f4852a76fae3fcd0b2a6a6b2a6c75c
Delvoid <delvoidltd@gmail.com>
2021-11-01 16:15:01 +0000

    file upload endpoint

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+var cors = require('cors')
 require('dotenv').config()
+const multer = require('multer')
+const path = require('path')
 
-var app = express();
+// storage location and file name
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+  },
+})
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+// upload middleware
+const upload = multer({ storage })
+
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file
+  if (!file) return res.status(400).send('Please upload a file')
+  let responseObject = {}
+  responseObject['name'] = req.file.originalname
+  responseObject['type'] = req.file.mimetype
+  responseObject['size'] = req.file.size
+  res.json(responseObject)
+})
 
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
